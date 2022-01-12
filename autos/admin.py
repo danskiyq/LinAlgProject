804c9dff241e330423e093f84088b4918c053912
DanilDanylchenko <danild@superplay.co>
2022-01-12 22:53:23 +0200

    admin form

--- autos/admin.py
+++ autos/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 from autos.models import Make, Auto
+from cats.models import Cat, Breed
 
 # Register your models here.
 
 admin.site.register(Make)
 admin.site.register(Auto)
+admin.site.register(Cat)
+admin.site.register(Breed)
